885faf51471adfd9f08eafb9d69e6c5899248aef
Sah2Sah2 <sara.battistella@iths.se>
2025-02-18 15:54:54 +0100

    Add CORS

--- Labb3CVApi/Program.cs
+++ Labb3CVApi/Program.cs
@@ -1,4 +1,3 @@
-
 using Labb3CVApi.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,19 +9,32 @@ namespace Labb3CVApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add CORS 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost",
+                    policy =>
+                    {
+                        policy.WithOrigins("https://localhost:5000") // Allow Blazor 
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
+                    });
+            });
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            // Add the DbContext
+            // Add DbContext
             builder.Services.AddDbContext<CvDbContext>(options =>
-            { 
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
             var app = builder.Build();
+
+            // Use CORS policy
+            app.UseCors("AllowLocalhost");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -34,7 +46,6 @@ namespace Labb3CVApi
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
